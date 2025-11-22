38ae47029086ed8bef4b91ca7c017604557c99c3
Andre-Benedetti <andbene@gmail.com>
2025-11-23 10:42:24 +1300

    Implement Fibonacci and factorial calculations
    Added a main function to calculate Fibonacci and factorial.

--- MSE 800 Act Week 1.py
+++ MSE 800 Act Week 1.py
@@ -1,18 +1,28 @@
-#activity 1
-name = "John Doe"
-Age = 28
-Skills = ["Python","SQL","Power Bi"]
-Education = ("BSC Computer Science", 2020)
-Contact_Details = {"email": "jdoe@gmail.com", "phone": "0222222222"} 
-Certifications = {"Azure", "AWS"}
+#activity 3 Fibonacci and Factorial
 
-#activity 2
+def main():
 
-#Assigning hours worked
-hours = 25
-#Assigning Payrate
-payrate = 31.2
-grosspay = hours * payrate
-#Priting the grosspay
+    Fibo = 1 #smallest Fiboacci element considering the series started as 0, 1
+    Factorial = 1 #smallest Factorial 
+    i = 1 # aux variables
+    
+    N = int(input("Enter N: ")) #enter from the user
 
-print (grosspay)
+    j = N # aux variables
+
+    while i < N: #calculating element N of Fibonacci series
+        Fibo = Fibo + i
+        i = Fibo - i
+
+    print (f"The Nth element of the Fibonacci series is: {Fibo}")
+
+    while j > 0: #calculating factorial
+        Factorial = j * Factorial
+        j=j-1
+
+    print (f"The Factorial of {N} is: {Factorial}")
+
+if __name__ == "__main__":
+
+    main()
+
