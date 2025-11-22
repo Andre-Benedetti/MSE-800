445642e48b9caefe15a367fa55b47819dfd0910d
Andre-Benedetti <andbene@gmail.com>
2025-11-23 09:15:06 +1300

    Add files via upload

--- MSE 800 Act Week 1.py
+++ MSE 800 Act Week 1.py
@@ -1,6 +1,17 @@
+#activity 1
 name = "John Doe"
 Age = 28
 Skills = ["Python","SQL","Power Bi"]
 Education = ("BSC Computer Science", 2020)
 Contact_Details = {"email": "jdoe@gmail.com", "phone": "0222222222"} 
 Certifications = {"Azure", "AWS"}
+
+#activity 2
+
+#Assigning hours worked
+hours = 25
+#Assingning Payrate
+payrate = 31.2
+grosspay = hours * payrate
+#Priting the grosspay
+print (grosspay)
