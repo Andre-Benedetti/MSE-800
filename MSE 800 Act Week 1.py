df688e08b51d77b9ad470c8deabae19744ce2ffb
Andre-Benedetti <andbene@gmail.com>
2025-11-23 08:38:00 +1300

    Add files via upload

---
+++ MSE 800 Act Week 1.py
@@ -0,0 +1,6 @@
+name = "John Doe"
+Age = 28
+Skills = ["Python","SQL","Power Bi"]
+Education = ("BSC Computer Science", 2020)
+Contact_Details = {"email": "jdoe@gmail.com", "phone": "0222222222"} 
+Certifications = {"Azure", "AWS"}
