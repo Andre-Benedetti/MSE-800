4a77b296b1d4da71541ece155671846c5ada3c33
Andre-Benedetti <andbene@gmail.com>
2025-11-29 12:00:03 +1300

    inclusion of requirements

--- W2-A2 code.py
+++ W2-A2 code.py
@@ -3,7 +3,6 @@ import math
 def main():
 
     N = int(input("Enter N: ")) #enter from the user
-
     
     phi = (1 + math.sqrt(5)) / 2 #aux variable
     nth_fib=round((phi ** N - (1 - phi) ** N) / math.sqrt(5)) #calculatin the N th term using Binet's formula
