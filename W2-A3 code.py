880a88cf36780f07f6ad81122ea6cab1c20a45a5
Andre-Benedetti <andbene@gmail.com>
2025-11-29 12:43:00 +1300

    Update W2-A3 code.py

--- W2-A3 code.py
+++ W2-A3 code.py
@@ -8,6 +8,15 @@ def fibonacci_recursive(n):
         # The nth Fibonacci number is the sum of the (n-1)th and (n-2)th
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
 
+def factorial_recursive(n):
+    
+    if n == 0: # factorial of 0 is 1
+        return 1
+   
+    else:
+        return n * factorial_recursive(n - 1)
+    
+
 def main():
 
     N = int(input("Enter N: ")) #input from the user
@@ -16,6 +25,7 @@ def main():
         print("Please enter a positive integer.")
     else:
         print (f"The {N}th element of the Fibonacci series is: {fibonacci_recursive(N)}")
+        print (f"The factorial of {N} is: {factorial_recursive(N)}")
 
 if __name__ == "__main__":
 
