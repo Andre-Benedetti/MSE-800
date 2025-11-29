115ac2b1646f3792a8d193e747963b1ad66e7460
Andre-Benedetti <andbene@gmail.com>
2025-11-30 09:00:37 +1300

    Update W2-A4 code.py

--- W2-A4 code.py
+++ W2-A4 code.py
@@ -1,24 +1,29 @@
 class Calculation:
 
-    if act == 0:
+    def __init__(self, N, act):
+        self.N = N
+        self.act = act
+
+
+    if self.act == 0:
         def fibonacci_recursive(n):
         
-            if n <= 0:
+            if self.N <= 0:
                 return 0  # The 0th Fibonacci number
-            elif n == 1:
+            elif self.N == 1:
                 return 1  # The 1st Fibonacci number
             else:
                 # The nth Fibonacci number is the sum of the (n-1)th and (n-2)th
-                return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+                return fibonacci_recursive(self.N - 1) + fibonacci_recursive(self.N - 2)
 
     else:
-        def factorial_recursive(n):
+        def factorial_recursive(self.N):
             
-            if n == 0: # factorial of 0 is 1
+            if self.N == 0: # factorial of 0 is 1
                 return 1
         
             else:
-                return n * factorial_recursive(n - 1)
+                return self.N * factorial_recursive(self.N - 1)
 
 
 def main():
