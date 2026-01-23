c9ae07ff716cc40cd1f5a2f5d54d7084f41f9115
Andre-Benedetti <andbene@gmail.com>
2026-01-24 12:24:53 +1300

    .

---
+++ W6-A2 code.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+class StudentDB: #create a class to handle database operations
+    def __init__(self, db_name="university.db"):
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self):# Create the students table if it doesn't already exist
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS students (
+                student_id TEXT PRIMARY KEY,
+                name TEXT,
+                score INTEGER
+            )
+        ''')
+        self.conn.commit()
+
+    def populate_data(self):# Initial data
+        
+        students_data = [
+            ('1', 'Joao', 35),
+            ('2', 'Maria', 90),
+            ('3', 'Pedro', 48),
+            ('4', 'Ana', 92),
+            ('5', 'Luis', 88)
+        ]
+        # Insert or update records
+        self.cursor.executemany('''
+            INSERT OR REPLACE INTO students (student_id, name, score)
+            VALUES (?, ?, ?)
+        ''', students_data)
+        self.conn.commit()
+
+    def get_top_students(self): # SQL query to retrieve the top N students ordered by score in descending order
+        query = "SELECT student_id, name, score FROM students ORDER BY score DESC LIMIT 3 "
+        self.cursor.execute(query)
+        return self.cursor.fetchall()
+
+    def close(self):# Close the database connection safely
+        self.conn.close()
+
+def main():
+    
+    db = StudentDB()
+    db.populate_data()
+
+    top_students = db.get_top_students()
+
+    print("Top 3 Students")
+    for student in top_students:
+        sid, name, score = student
+        print(f"ID: {sid} | Name: {name:<6} | Score: {score}")
+
+    # Ensure the connection is closed after execution
+    db.close()
+
+if __name__ == "__main__":
+    main()
