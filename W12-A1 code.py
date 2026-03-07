f24ef6aa5fcccc5c5ec8c038e859fd04c312edc8
Andre-Benedetti <andbene@gmail.com>
2026-03-08 10:31:18 +1300

    .

--- W12-A1 code.py
+++ W12-A1 code.py
@@ -7,13 +7,24 @@ def hello_world():
     logo_url = url_for('static', filename='flask-logo.svg')
     
     return f"""
-    <h1>Welcome to my Flask App!</h1>
-    <p>Hello Flask Framework!</p>
-    
-    <p>Check out the <a href="https://flask.palletsprojects.com/en/stable/quickstart/">Flask Quickstart Guide</a>.</p>
-    
-    <p>Here is the local Flask logo:</p>
-    <img src="{logo_url}" alt="Flask Logo" width="200">
+   <div style="background-color: green; color: red; min-height: 100vh; padding: 20px; font-family: sans-serif;">
+        <h1>Welcome to my Flask App!</h1>
+        <p>Hello Flask Framework!</p>
+        
+        <p>Check out the <a href="https://flask.palletsprojects.com/en/stable/quickstart/" style="color: yellow;">Flask Quickstart Guide</a>.</p>
+        
+        <hr style="border: 0.5px solid white; margin: 20px 0;">
+
+        <h3>File Browser</h3>
+        <p>Use the link below to open the logo file:</p>
+        
+        <a href="{logo_url}" target="_blank" style="color: #ADFF2F; font-weight: bold; font-size: 1.2em;">
+            Browse Image
+        </a>
+
+        <br><br>
+        
+    </div>
     """
 
 @app.route("/bye")
@@ -26,7 +37,12 @@ def learner(name):
 
 @app.route("/username/<name>/<int:number>")
 def learner2(name, number):
-    return f"{name} is learning FLASK!{name} wakes up at {number} a.m. every day</p>"
+    return f"""
+    <div style="background-color: green; color: white; min-height: 100vh; padding: 20px; font-family: sans-serif;">
+        <h1>Welcome to my Flask App!</h1>
+        <p>{name} is learning FLASK!{name} wakes up at {number} a.m. every day</p>
+    </div>
+    """
 
 if __name__ == "__main__":
     app.run(debug=True)
